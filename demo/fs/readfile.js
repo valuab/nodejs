15542455b83c6a8eaa5d47711d7d4f52e2bbaf21
valuab <1363907445@qq.com>
2020-12-11 11:39:42 +0800

    提交

--- demo/fs/readfile.js
+++ demo/fs/readfile.js
@@ -2,5 +2,20 @@
  * @Author: YJ
  * @Date: 2020-12-07 12:00:47
  * @
-# Description: 
+# Description: 读取文件
  */
+const fs = require('fs')
+
+// 默认异步
+const fileName1 = fs.readFile('./file.txt','UTF8',(err,data)=>{
+  if(err) throw err;
+
+  console.log(data);// Buffer 对象
+
+  console.log();
+})
+
+const fileName2 = fs.readFileSync('./file.json','UTF8')
+console.log(fileName2);
+
+// 长文件过大读取可以一步步走
